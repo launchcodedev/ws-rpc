7cae7bec10940cecc954d311a9edbc5db74cf2de
Joel Gallant <joel@joelgallant.me>
2021-01-01 20:26:47 -0700

    feat: tests http and https servers

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,8 @@ import { unlinkSync } from 'fs';
 import { resolve } from 'path';
 import WS from 'ws';
 import ReconnectingWS from 'reconnecting-websocket';
+import http from 'http';
+import https from 'https';
 
 import { jsonSerialization, build, Builder } from './index';
 import bsonSerialization from './bson';
@@ -378,3 +380,96 @@ describe('unix sockets', () => {
     }
   });
 });
+
+describe('http(s) servers', () => {
+  it('connects using an http server', async () => {
+    const port = await getPort();
+    const httpServer = http.createServer();
+    httpServer.listen(port);
+
+    const common = build(jsonSerialization);
+    const server = await common.server({}, false).listen(httpServer);
+    const client = await common.client(false).connect(port);
+
+    try {
+      await client.ping();
+      await server.ping();
+    } finally {
+      await server.close();
+      await client.close();
+    }
+  });
+
+  it('connects using an https server', async () => {
+    const port = await getPort();
+    const httpsServer = https.createServer({ cert: selfSignedCert, key: selfSignedKey });
+    httpsServer.listen(port);
+
+    const wsClient = new WS(`wss://localhost:${port}`, { ca: [selfSignedCert] });
+
+    const common = build(jsonSerialization);
+    const server = await common.server({}, false).listen(httpsServer);
+    const client = await common.client(false).connect(wsClient);
+
+    try {
+      await client.ping();
+      await server.ping();
+    } finally {
+      await server.close();
+      await client.close();
+    }
+  });
+});
+
+const selfSignedCert = `
+-----BEGIN CERTIFICATE-----
+MIIDOzCCAiOgAwIBAgIJYhn5rcdHL6V/MA0GCSqGSIb3DQEBCwUAMBQxEjAQBgNV
+BAMTCWxvY2FsaG9zdDAeFw0yMTAxMDIwMzI1MDRaFw0zMDEyMzEwMzI1MDRaMBQx
+EjAQBgNVBAMTCWxvY2FsaG9zdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBAPVv2uJZm8AVCmCT74x+1iLTGkGUqBevjWAIi/ynpDt6nESb6MjKpQV0zc8S
+fbFJK/8PWMS7xlllRRrEYMbLWLE3CvobYPOMU83JgEBJpDT0lV74kOSzEdvpqnQA
+EVpB0FEAQ1sZgo1YmrRq1mB/C/Vqs0JQH4NUwc46gjEN5B/EyuNMeQVoZpnJvPCv
+3SgSU2X3vi+a6n/HaDMMH/cA5gp9USVBLGFDnuRxnNOumac7a3VvSjMrZqwEpRBC
+tgMCvfkC8RfAzgyoh+xFjnb6aJwbEhLsxp7oTYDXyLP1FImVLyF5b6RM2Bd6VWmh
+XFBTuuv8jD9VI0BmG7Lel6pOtAcCAwEAAaOBjzCBjDALBgNVHQ8EBAMCAvQwMQYD
+VR0lBCowKAYIKwYBBQUHAwEGCCsGAQUFBwMCBggrBgEFBQcDAwYIKwYBBQUHAwgw
+SgYDVR0RBEMwQYIJbG9jYWxob3N0ghVsb2NhbGhvc3QubG9jYWxkb21haW6CBVs6
+OjFdhwR/AAABhxD+gAAAAAAAAAAAAAAAAAABMA0GCSqGSIb3DQEBCwUAA4IBAQCS
+todGx7DaaYyGUsNX0l+mS9KBgf8GEbt0kvpu9lHKJfw5vvA2LnLEzrBCI3BO/b+Q
+KBjAktkK84iPuSZ2OIpodr/z0yIyElBwLK3273ccinlXeEbrshBwFp8SFTf/SAcL
+ZGTH5HGySZQM4Lel+XJ7VpDzAe2iqT1H/vRlV9l0t8srNOWQbOqhvg/NCREKYOWQ
+mjpNj8QCSBuvQphH5GuNyNVA07G4hXOBezWwhEwcq1HJrDJnBF/jtSqTjpegeNqT
+DyXMsoKTfPPzlV9dBNRmGcxF4r4Mus1/lDmhpTEpIg0YNfRFUKCSH7TWzRhN9LWP
+Qmqe4aTR6sntE8otYLhE
+-----END CERTIFICATE-----
+`;
+
+const selfSignedKey = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEA9W/a4lmbwBUKYJPvjH7WItMaQZSoF6+NYAiL/KekO3qcRJvo
+yMqlBXTNzxJ9sUkr/w9YxLvGWWVFGsRgxstYsTcK+htg84xTzcmAQEmkNPSVXviQ
+5LMR2+mqdAARWkHQUQBDWxmCjViatGrWYH8L9WqzQlAfg1TBzjqCMQ3kH8TK40x5
+BWhmmcm88K/dKBJTZfe+L5rqf8doMwwf9wDmCn1RJUEsYUOe5HGc066ZpztrdW9K
+MytmrASlEEK2AwK9+QLxF8DODKiH7EWOdvponBsSEuzGnuhNgNfIs/UUiZUvIXlv
+pEzYF3pVaaFcUFO66/yMP1UjQGYbst6Xqk60BwIDAQABAoIBAAMV/PKLneG0YgUE
+1yY1EgwuC053yAVEN8rVUK1EjlQRHpXeP/cGVTzUUyIfYWUxPlRepQcUNVI6a7wC
+bBUTyXGw93pdjcKCKSuVNP+Z7W6dBKPFDE1T0w2oynPa7FzuJuhd0Hr8vx931boe
+/cMI1eWoCcjzqPHFxwoIwkpLmmqr0HOTUF0C7Z2RIo7zNQoh9OLhaC/6pZ7JhAtt
+J4+GCZPEIVrqVKtmBaHJiN+xtmR+eOMp3s3pjBbwfzwPGPrwEicgMmqgfghpRTMP
+4RxlqgcAuKrBv94mKMuwCbm37Mkkx9DlOLFioqQ0d8m9CGV1qN5Stg/thVIe66h6
+Xr2nr0ECgYEA+7KfFm+tbTFalw2mB4zEmTB4l2SrcRtqUzKTsKoOBDcCqenCVuGE
+6lBrmREic2DTlWGbfp9dJzhu1px1r/T24VWKzFRlX2e2TLdMpba9PrUmRk13iWQY
+csFyIYckBImF2MLpBhV3FIZIrSQocT+pjzBaIIaix1Vq4X65xu8BMm0CgYEA+aHW
+afCrPtiqwnFnCqCWHB+oc2kfcX18g0+hwlofRysuf8MMogKjC6C1p2rA8CcRqNTV
+q1WPfW+svh/Qbc+Vdvw3CwgLsXhswo9bB7umCqCCkYaSP78nLDF32tLUAz21kEGo
+xfO0M8WqNANabgTXbZo2L0BIVw7x+cTpaFXjl8MCgYEA2H7MziyjHMAN7s3jmKzh
+Ue7aW0ZRHQn6y7M+TAAJ7GAw31vdOIPkovMnidKuMlX/yIbbi++h3aFx3RFZPU9U
+p3+/0n9pbsWzjYtA1202nGCOmnv5rOi3CsYP2Hz4YxqzUT5d10jRU4spqhvm2Xpr
+62korL+B9jknpOwu+ckM24UCgYAZODVuPIdAcSlHPae4ViL0MmqVRlCL6a6ToY7p
+EKvKR92JwM3c7EFGonTXthxJ5tiM4vu3NIyrkoW0K4imH2utOqvg7G4p/s2WFl+O
+93E975thUmQiFpDBkTnXnKcYsLpQGaIQZZ+V/2lDmfcf2FNfWk4RFgB48ySVJESk
+atD0ewKBgQDK2m3yl0Iy7oCCmaG2Nrikjnq8LlJUJ95hpHV+EpQzHMwhjMlTNvfu
+D28fsb2p9IUjRaXwMEar+3p6CCx38xeVC9lGX+mhRZ33Kh0Jv2oL7wlqqoZsycl3
+K5TMnca38A5sR4o90XUNgoM6FnIB8MnW04B6Zhv3ijbGLEjcRiXjow==
+-----END RSA PRIVATE KEY-----
+`;
